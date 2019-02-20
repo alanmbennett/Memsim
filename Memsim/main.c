c14ff37102b3eca4a75bd6ce2a376125ef79780f
Alan B <alanmitchellbennett@gmail.com>
2019-02-20 12:07:55 -0500

    Initial Commit

---
+++ Memsim/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Memsim
+//
+//  Created by Alan Bennett on 2/20/19.
+//  Copyright © 2019 Alan Bennett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
