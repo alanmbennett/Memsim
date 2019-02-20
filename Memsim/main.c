f329bc271ccccd16b1c041fa361b29104df71224
Alan B <alanmitchellbennett@gmail.com>
2019-02-20 12:22:28 -0500

    Creation of main .c file

--- Memsim/main.c
+++ Memsim/main.c
@@ -1,15 +1,6 @@
-//
-//  main.c
-//  Memsim
-//
-//  Created by Alan Bennett on 2/20/19.
-//  Copyright © 2019 Alan Bennett. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, const char * argv[])
+{
+
 }
